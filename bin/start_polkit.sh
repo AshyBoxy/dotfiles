af3c7fa3baa606f9931a213e444c50bf40f7414f
AshyBoxy <ashyboxy@ashyboxy.xyz>
2025-10-19 03:37:05 +0100

    fix start_polkit.sh

--- bin/start_polkit.sh
+++ bin/start_polkit.sh
@@ -3,5 +3,6 @@ if  pgrep -x hyprpolkitagena ; then
     echo hyprpolkitagent already loaded
 else
     echo starting hyprpolkitagent
-    exec /usr/lib/hyprpolkitagent/hyprpolkitagent
+    # it seems to die without having outputs attached?
+    exec /usr/lib/hyprpolkitagent/hyprpolkitagent >/dev/null 2>/dev/null
 fi
